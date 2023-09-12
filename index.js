e18be2b10057bc7bd5932ea8eddfc97755891645
Sami <s.babouche@esi-sba.dz>
2023-09-12 01:10:57 +0100

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+// code your solution here 
+const record = [ { year: "2015", result: "x"}, { year: "2014", result: "N/A"}, { year: "2013", result: "L"},  ]
+ function superbowlWin(array){
+     function isWin(element){ 
+        return element.result==="W" } 
+     if (array.find(isWin)){const obj=array.find(isWin); return obj.year } 
+     else{return undefined} } 
